70cd70c690498c989e718d947e7d040232b4f010
Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>
2013-05-29 21:56:38 -0700

    msm: mdss: support wfd and rotation simultaneously
    Concurrent rotation and WFD usecases need to be supported
    using a single writeback hw module for low end targets.
    To enable this feature, mixer, ctl and DMA pipe needs to be
    shared between block and line mode operations and hardware
    programing is synchronized with a shared mutex.
    
    Change-Id: I0fe909dd07006282239c65d5fe92d270069a3920
    Signed-off-by: Sree Sesha Aravind Vadrevu <svadrevu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp.h
+++ drivers/video/msm/mdss/mdss_mdp.h
@@ -118,6 +118,11 @@ struct mdss_mdp_vsync_handler {
 	struct list_head list;
 };
 
+enum mdss_mdp_wb_ctl_type {
+	MDSS_MDP_WB_CTL_TYPE_BLOCK = 1,
+	MDSS_MDP_WB_CTL_TYPE_LINE
+};
+
 struct mdss_mdp_ctl {
 	u32 num;
 	char __iomem *base;
@@ -151,6 +156,7 @@ struct mdss_mdp_ctl {
 	struct mdss_mdp_mixer *mixer_left;
 	struct mdss_mdp_mixer *mixer_right;
 	struct mutex lock;
+	struct mutex *shared_lock;
 
 	struct mdss_panel_data *panel_data;
 	struct mdss_mdp_vsync_handler vsync_handler;
@@ -168,6 +174,7 @@ struct mdss_mdp_ctl {
 					struct mdss_mdp_vsync_handler *);
 
 	void *priv_data;
+	u32 wb_type;
 };
 
 struct mdss_mdp_mixer {
@@ -561,6 +568,10 @@ int mdss_mdp_limited_lut_igc_config(struct mdss_mdp_ctl *ctl);
 int mdss_mdp_calib_config(struct mdp_calib_config_data *cfg, u32 *copyback);
 int mdss_mdp_calib_config_buffer(struct mdp_calib_config_buffer *cfg,
 						u32 *copyback);
+
+int mdss_mdp_writeback_display_commit(struct mdss_mdp_ctl *ctl, void *arg);
+struct mdss_mdp_ctl *mdss_mdp_ctl_mixer_switch(struct mdss_mdp_ctl *ctl,
+					       u32 return_type);
 
 int mdss_mdp_wb_set_format(struct msm_fb_data_type *mfd, int dst_format);
 int mdss_mdp_wb_get_format(struct msm_fb_data_type *mfd,

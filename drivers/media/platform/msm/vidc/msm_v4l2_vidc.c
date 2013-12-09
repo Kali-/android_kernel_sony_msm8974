79d5dafc441d994df73da7f68ebbbabb3ef1d117
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-12-09 12:59:05 +0100

    Revert "msm: vidc: Add logic to unregister input buffers"
    This reverts commit dd0d60dc457c7a0b5a0170d8aef2cb21f034f00e.

--- drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
+++ drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
@@ -254,8 +254,7 @@ fail_mem_client:
 fail_nomem:
 	return rc;
 }
-static int msm_v4l2_release_buffers(struct msm_v4l2_vid_inst *v4l2_inst,
-				int buffer_type)
+static int msm_v4l2_release_output_buffers(struct msm_v4l2_vid_inst *v4l2_inst)
 {
 	struct list_head *ptr, *next;
 	struct buffer_info *bi;
@@ -265,7 +264,7 @@ static int msm_v4l2_release_buffers(struct msm_v4l2_vid_inst *v4l2_inst,
 	int i;
 	list_for_each_safe(ptr, next, &v4l2_inst->registered_bufs) {
 		bi = list_entry(ptr, struct buffer_info, list);
-		if (bi->type == buffer_type) {
+		if (bi->type == V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE) {
 			buffer_info.type = bi->type;
 			for (i = 0; (i < bi->num_planes)
 				&& (i < VIDEO_MAX_PLANES); i++) {
@@ -311,8 +310,7 @@ static int msm_v4l2_close(struct file *filp)
 	int i;
 	vidc_inst = get_vidc_inst(filp, NULL);
 	v4l2_inst = get_v4l2_inst(filp, NULL);
-	rc = msm_v4l2_release_buffers(v4l2_inst,
-			V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE);
+	rc = msm_v4l2_release_output_buffers(v4l2_inst);
 	if (rc)
 		dprintk(VIDC_WARN,
 			"Failed in %s for release output buffers\n", __func__);
@@ -385,7 +383,7 @@ int msm_v4l2_reqbufs(struct file *file, void *fh,
 	int rc = 0;
 	v4l2_inst = get_v4l2_inst(file, NULL);
 	if (b->count == 0)
-		rc = msm_v4l2_release_buffers(v4l2_inst, b->type);
+		rc = msm_v4l2_release_output_buffers(v4l2_inst);
 	if (rc)
 		dprintk(VIDC_WARN,
 			"Failed in %s for release output buffers\n", __func__);
@@ -652,8 +650,7 @@ static int msm_v4l2_decoder_cmd(struct file *file, void *fh,
 	int rc = 0;
 	v4l2_inst = get_v4l2_inst(file, NULL);
 	if (dec->cmd == V4L2_DEC_CMD_STOP)
-		rc = msm_v4l2_release_buffers(v4l2_inst,
-				V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE);
+		rc = msm_v4l2_release_output_buffers(v4l2_inst);
 	if (rc)
 		dprintk(VIDC_WARN,
 			"Failed to release dec output buffers: %d\n", rc);
@@ -668,8 +665,7 @@ static int msm_v4l2_encoder_cmd(struct file *file, void *fh,
 	int rc = 0;
 	v4l2_inst = get_v4l2_inst(file, NULL);
 	if (enc->cmd == V4L2_ENC_CMD_STOP)
-		rc = msm_v4l2_release_buffers(v4l2_inst,
-				V4L2_BUF_TYPE_VIDEO_CAPTURE_MPLANE);
+		rc = msm_v4l2_release_output_buffers(v4l2_inst);
 	if (rc)
 		dprintk(VIDC_WARN,
 			"Failed to release enc output buffers: %d\n", rc);
